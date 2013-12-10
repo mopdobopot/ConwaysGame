9f35476b1676b87936757901019eae25f57379b1
mopdobopot <lommovtsev@gmail.com>
2013-12-10 17:26:10 +0600

    init

---
+++ app.js
@@ -0,0 +1,27 @@
+/**
+ * Created by NKS-USER on 10.12.13.
+ */
+$(document).ready(function() {
+
+    var map = [[]],
+        neighborhood = [[]],
+        oldAlive = [],
+        newAlive = [],
+        windRose = [
+            {x: 0 , y: -1},
+            {x: -1, y: -1},
+            {x: -1, y: 0 },
+            {x: -1, y: 1 },
+            {x: 0 , y: 1 },
+            {x: 1 , y: 1 },
+            {x: 1 , y: 0 },
+            {x: 1 , y: -1}
+        ],
+
+        redraw = function() {
+            oldAlive.forEach(function(cell) {
+
+            });
+        };
+
+});
